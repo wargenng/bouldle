f98c4ae10cdc574d2acdac8114d4bb95c9e32b62
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-17 12:54:55 -0700

    adding tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import styles from "./App.module.css";
-
 function App() {
-    return <div class={styles.App}>hello!</div>;
+    return (
+        <div class="flex align-center justify-center p-8">
+            <h1>bouldle.</h1>
+            <div class="h-60 w-60 overflow-hidden rounded-full flex items-center justify-center">
+                <img
+                    class="blur-none w-full h-auto"
+                    src="https://mountainproject.com/assets/photos/climb/107547798_medium_1494196396.jpg?cache=1711577257"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
