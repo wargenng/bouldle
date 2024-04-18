f0289c4e394bb1c1fe3628a9d29256a5986249a1
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-18 14:51:19 -0700

    removed decimals

--- src/components/haversine.js
+++ src/components/haversine.js
@@ -10,7 +10,7 @@ export function haversine(lat1, lon1, lat2, lon2) {
         Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
         Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = Math.round(R * c * 100) / 100; // in meters
+    const distance = Math.round(R * c); // in meters
 
     // Calculate direction
     const y = Math.sin(Δλ) * Math.cos(φ2);
