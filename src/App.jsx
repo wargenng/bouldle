e1a3d55c11066b08499d17c925b417571c81e382
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-17 23:52:21 -0700

    added blur at start

--- src/App.jsx
+++ src/App.jsx
@@ -57,9 +57,9 @@ function App() {
             <div class="w-full flex-col items-center justify-center">
                 <h1 class="text-2xl pb-4">bouldle.</h1>
                 <div class="w-full flex items-center justify-center pb-4">
-                    <div class="h-72 w-72 overflow-hidden flex items-center justify-center object-cover">
+                    <div class="h-72 w-72 overflow-hidden flex items-center justify-center object-cover border-black border-4">
                         <img
-                            class="blur-none w-full h-auto"
+                            class="w-full h-auto blur-2xl"
                             src={climbData.climbs[currentDay].image}
                         />
                     </div>
