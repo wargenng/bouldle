c4ce09e92bdc2323ecd160792328276e945ab4c1
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-20 00:31:13 -0700

    refactoring information

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Select, createOptions } from "@thisbeyond/solid-select";
 import "@thisbeyond/solid-select/style.css";
 import "./styles/select.css";
 import { ConfettiExplosion } from "solid-confetti-explosion";
+import Information from "./components/Information";
 
 const blurAmountList = [25, 10, 5, 4, 3, 2, 1, 0];
 const allowedGuesses = blurAmountList.length;
@@ -79,42 +80,7 @@ function App() {
                     onclick={handleClose}
                 />
                 <div class="absolute w-3/4 h-fit bg-white rounded-lg shadow-md flex flex-col justify-center">
-                    <div class="m-4 text-xl">
-                        <h1 class="font-bold text-2xl mb-2">how to play</h1>
-                        <p class="mb-2">
-                            1. Try to guess the red rock boulder in 8 guesses.
-                        </p>
-                        <p class="mb-2">
-                            2.{" "}
-                            <span class="text-green-500 font-bold">Green</span>{" "}
-                            is an exact match.
-                        </p>
-                        <p class="mb-2">
-                            3.{" "}
-                            <span class="text-yellow-300 font-bold">
-                                Yellow
-                            </span>{" "}
-                            is a close match. A close...
-                        </p>
-                        <p class="ml-4 mb-2">
-                            A. <span class="font-bold">Grade</span>,{" "}
-                            <span class="font-bold">Distance </span>
-                            is off by at most 1.
-                        </p>
-                        <p class="ml-4 mb-2">
-                            B. <span class="font-bold">Length</span> is off by 2
-                            ft.
-                        </p>
-                        <p class="ml-4 mb-2">
-                            C. <span class="font-bold">Stars</span> is off by
-                            0.5 points.
-                        </p>
-                        <p class="ml-4 mb-2">
-                            D. <span class="font-bold">Votes</span> is off by 50
-                            point.
-                        </p>
-                        <p>4. New climb is available at midnight local time.</p>
-                    </div>
+                    <Information />
                     <button
                         class="bg-red-600 text-white p-2 m-4 rounded-lg font-bold"
                         onclick={handleClose}
---
+++ src/components/Information.jsx
@@ -0,0 +1,33 @@
+export default function Information(props) {
+    return (
+        <div class="m-4 text-xl">
+            <h1 class="font-bold text-2xl mb-2">how to play</h1>
+            <p class="mb-2">
+                1. Try to guess the red rock boulder in 8 guesses.
+            </p>
+            <p class="mb-2">
+                2. <span class="text-green-500 font-bold">Green</span> is an
+                exact match.
+            </p>
+            <p class="mb-2">
+                3. <span class="text-yellow-300 font-bold">Yellow</span> is a
+                close match. A close...
+            </p>
+            <p class="ml-4 mb-2">
+                A. <span class="font-bold">Grade</span>,{" "}
+                <span class="font-bold">Distance </span>
+                is off by at most 1.
+            </p>
+            <p class="ml-4 mb-2">
+                B. <span class="font-bold">Length</span> is off by 2 ft.
+            </p>
+            <p class="ml-4 mb-2">
+                C. <span class="font-bold">Stars</span> is off by 0.5 points.
+            </p>
+            <p class="ml-4 mb-2">
+                D. <span class="font-bold">Votes</span> is off by 50 point.
+            </p>
+            <p>4. New climb is available at midnight local time.</p>
+        </div>
+    );
+}
