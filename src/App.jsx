477934ed4315675ad5f51eb79e12c989c23fa12b
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-20 01:14:27 -0700

    added information

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,29 @@ function App() {
                 </div>
             </div>
             <div class="w-full flex-col items-center justify-center p-6">
-                <h1 class="text-2xl pb-4">bouldle.</h1>
+                <div class="w-full flex pb-4">
+                    <h1 class="text-2xl font-bold">bouldle.</h1>
+                    <div class="grow" />
+                    <svg
+                        fill="currentColor"
+                        stroke-width="0"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        height="1.4rem"
+                        width="1.4rem"
+                        style="overflow: visible; color: currentcolor;"
+                        class="mt-1"
+                        onclick={() => {
+                            setShowInfo(true);
+                        }}
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M8.568 1.031A6.8 6.8 0 0 1 12.76 3.05a7.06 7.06 0 0 1 .46 9.39 6.85 6.85 0 0 1-8.58 1.74 7 7 0 0 1-3.12-3.5 7.12 7.12 0 0 1-.23-4.71 7 7 0 0 1 2.77-3.79 6.8 6.8 0 0 1 4.508-1.149zM9.04 13.88a5.89 5.89 0 0 0 3.41-2.07 6.07 6.07 0 0 0-.4-8.06 5.82 5.82 0 0 0-7.43-.74 6.06 6.06 0 0 0 .5 10.29 5.81 5.81 0 0 0 3.92.58zM7.375 6h1.25V5h-1.25v1zm1.25 1v4h-1.25V7h1.25z"
+                            clip-rule="evenodd"
+                        ></path>
+                    </svg>
+                </div>
                 <div class="w-full flex items-center justify-center pb-4">
                     <div class="h-72 w-72 overflow-hidden flex items-center justify-center object-cover shadow-lg">
                         <img
