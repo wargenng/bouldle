7c8a31eb2d6e46a5c56ad9fb9eb343a1b0c94ff6
Austin Merrick <austinmerrick@onsclom.net>
2024-04-26 18:07:09 -0700

    Remove warn modal

--- src/components/guess/guess.jsx
+++ src/components/guess/guess.jsx
@@ -2,13 +2,10 @@ import climbData from "../../../scripts/data.json";
 import { random } from "../../utilities/random";
 import { getCurrentDateFormattedAsInt } from "../../utilities/getCurrentDateFormattedAsInt";
 import { createSignal } from "solid-js";
-import Warn from "../warn";
 import Details from "./components/details";
 import { compareGuessToAnswer } from "./utilities/compareGuessToAnswer";
 
 export default function Guess(props) {
-    const [showWarn, setShowWarn] = createSignal(false);
-
     const guess =
         climbData.climbs[
             climbData.climbs.map((climb) => climb.route).indexOf(props.guess)
@@ -18,12 +15,6 @@ export default function Guess(props) {
 
     return (
         <div class="text-white px-6">
-            <Warn
-                showWarn={showWarn()}
-                setShowWarn={setShowWarn}
-                route={guess.route}
-                link={guess.link}
-            />
             <div class=" text-center mb-5">
                 <div class="flex items-center gap-4 mb-3">
                     <div class="h-20 w-20 rounded-full overflow-hidden object-cover flex items-center justify-center">
@@ -32,14 +23,12 @@ export default function Guess(props) {
                             class="min-w-full min-h-full shrink"
                         />
                     </div>
-                    <p
+                    <a
                         class="text-xl font-bold underline text-primary"
-                        onclick={() => {
-                            setShowWarn(true);
-                        }}
+                        href={guess.link}
                     >
                         {guess.route}
-                    </p>
+                    </a>
                 </div>
                 <div class="grid grid-cols-12 gap-3">
                     {details.map((detail) => (
