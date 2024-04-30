641c0f30f7dd66d610c888be00b6faf303fccceb
Austin Merrick <austinmerrick@onsclom.net>
2024-04-29 21:33:08 -0700

    Simplify guess animation logic

--- src/components/guess/components/details.jsx
+++ src/components/guess/components/details.jsx
@@ -1,19 +1,27 @@
+import { createSignal, onMount } from "solid-js";
+import { delay } from "../../../utilities/delay";
+
 export default function Details(props) {
+    const [showResult, setShowResult] = createSignal(!props.shouldAnimate);
+    if (props.shouldAnimate) {
+        onMount(async () => {
+            await delay(props.delay)
+            setShowResult(true);
+            props.onAnimateEnd && props.onAnimateEnd();
+        })
+    }
     return (
         <div
-            class={`p-2 rounded-lg flex flex-col items-center ${
-                props.colSpan
-            } ${
-                props.isAnimated || props.isMount
+            class={`p-2 rounded-lg flex flex-col items-center ${props.colSpan
+                } ${showResult()
                     ? `${props.bgColor} border-background`
                     : `bg-background border-primary/20`
-            } ${
-                props.isMount && !props.isAnimated ? "animate-pop-option" : null
-            } border`}
+                } ${showResult() && props.shouldAnimate ? "animate-pop-option" : null
+                } border`}
         >
             <div class="text-sm font-medium">{props.title}</div>
             <div class="text-lg font-bold h-full flex items-center">
-                {props.isAnimated || props.isMount
+                {showResult()
                     ? props.value
                     : props.defaultValue}
             </div>
